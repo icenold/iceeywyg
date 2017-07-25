e0e67aee08450cad4eaae500fd3f643888f8531a
zyarnold <arnoldn@zylun.com>
2017-07-25 21:00:00 +0800

    initial setup for ngModel

--- src/app/iceey-editor/iceey-editor.component.ts
+++ src/app/iceey-editor/iceey-editor.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Provider, Input, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+
+export const iceeywygValueAccessor: any = {
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => IceeyEditorComponent),
+    multi: true
+};
 
 @Component({
-  selector: 'app-iceey-editor',
+  selector: 'iceeywyg',
   templateUrl: './iceey-editor.component.html',
-  styleUrls: ['./iceey-editor.component.css']
+  styleUrls: ['./iceey-editor.component.css'],
+  providers: [iceeywygValueAccessor]
 })
-export class IceeyEditorComponent implements OnInit {
+export class IceeyEditorComponent implements OnInit, ControlValueAccessor {
 
   constructor() { }
 
   ngOnInit() {
+  }
+  //stringModel - used only for listening changes to ngModel via writeValue(_val)
+  stringModel:string = ``;
+
+  //empty callbacks
+  private onTouchedCallback = ()=>{};
+  private onChangeCallback = (_newVal)=>{};
+
+  //register empty callbacks
+  registerOnChange(fn: any) {
+      this.onChangeCallback = fn;
+  }
+  registerOnTouched(fn: any) {
+      this.onTouchedCallback = fn;
+  }
+  
+  //accessors
+  writeValue(_val:string){
+    this.stringModel =  _val;
+  }
+  broadcastInput(_val){
+    this.onChangeCallback(_val);
   }
 
 }
